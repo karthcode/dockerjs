f242cc6021687a5458724ebd1f568cb64daab2d1
karthcode <skoreddy05@gmail.com>
2024-11-13 15:32:25 +0530

    Delete app.js.js

--- app.js.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/',(req,res) => {
-res.send("Docker Nodejs Example");
-});
-app.listen(3111,() => {
-console.log("Server is running on port 3111")
-});
